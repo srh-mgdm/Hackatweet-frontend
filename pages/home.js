96ec3d4a85df67ece244759d21b329d343bc69db
julien <julien77cojean@gmail.com>
2024-10-10 17:29:17 +0200

    update/tweet/trend/home

--- pages/home.js
+++ pages/home.js
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
-import Tweet from '../components/Tweet';
-
-
-function Home() {
-  return <Tweet/>;
-}
-
-export default Home;
-=======
 import Home from '../components/Home';
-import Login from '../components/Login'
+
 
 function home() {
-  return <div>
-  <Home/>
-
-  </div>
+  return <Home/>;
 }
 
 export default home;
->>>>>>> cd8a2f70c3be507ec8f98c770a739be2611e4c05
