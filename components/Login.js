dd9a1a31bf923eb2b468ab042012d4afd8a00fb6
costa-rica <nrodrig1@gmail.com>
2024-10-10 14:32:37 +0200

    refactor: signup et enregistre ça marche

--- components/Login.js
+++ components/Login.js
@@ -1,12 +1,15 @@
 import styles from '../styles/Login.module.css'
 import Image from 'next/image';
 import { Button, Modal } from 'antd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function Login() {
 
     const [firstName, firstNameSetter] = useState('');
+    const [username, usernameSetter] = useState('');
+    const [password, passwordSetter] = useState('');
+    const [clickSignUpBool, clickSignUpBoolSetter] = useState(false);
 
     // Pour le modal
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -19,6 +22,37 @@ function Login() {
     const handleCancel = () => {
         setIsModalOpen(false);
     };
+
+    const clickSignUp = () => {
+        console.log(`-on a cliqué 📢`)
+        console.log(`firstName: ${firstName}`)
+        console.log(`username: ${username}`)
+        console.log(`password: ${password}`)
+        clickSignUpBoolSetter(true)
+    }
+
+
+    useEffect(() => {
+        if (clickSignUpBool){
+            const bodyData = {
+                firstName: firstName,
+                username: username,
+                password: password,
+            }
+
+            fetch('http://localhost:3000/users/signup', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(bodyData)
+            })
+              .then(response => response.json())
+              .then(data => {
+                console.log(`reponse bien reçu 🎉`)
+                console.log(`data: ${data.result}`)
+              });
+        }
+
+      }, [clickSignUpBool]);
 
 
 
@@ -38,23 +72,33 @@ function Login() {
                     </div>
                     <div>already have an account?</div>
                     <div className={styles.divUnButon}>
-                        <button className={styles.btnLogin}>Sign ip </button>
+                        <button className={styles.btnLogin}>Sign in </button>
                     </div>
                 </div>
 
             </div>
 
 
-            <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                <div className={styles.mainSignUpModal}>
-                <input onChange={(e) => firstNameSetter(e.target.value)} value={firstName}/>
+            <Modal title="Sign Up" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}
+                footer={<></>}
+            >
 
-                <button className={styles.btnLogin}>Sign ip </button>
+                <div className={styles.mainSignUpModal}>
+                    <div className={styles.divModalInput}>
+                        <input placeholder='firstName' onChange={(e) => firstNameSetter(e.target.value)} />
+                    </div>
+                    <div className={styles.divModalInput}>
+                        <input placeholder='username' onChange={(e) => usernameSetter(e.target.value)} />
+                    </div>
+                    <div className={styles.divModalInput}>
+
+                        <input placeholder='password' onChange={(e) => passwordSetter(e.target.value)} />
+                    </div>
+
+                    <button className={styles.btnLogin} onClick={() => clickSignUp()}>Sign up </button>
                 </div>
 
             </Modal>
-
-
 
         </div>
     )
