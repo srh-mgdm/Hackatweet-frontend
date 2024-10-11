66c0587b9db692a0c64557913ac554cd8e8b18a6
julien <julien77cojean@gmail.com>
2024-10-11 10:32:08 +0200

    style.css

--- components/Home.js
+++ components/Home.js
@@ -1,22 +1,38 @@
 import styles from '../styles/Home.module.css';
 import Image from 'next/image';
-import { useEffect, useState,useSelector } from 'react';
+import { useEffect, useState,} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Tweet from './Tweet'
 import Trends from './Trends';
-
+import {logout} from '../reducers/user'
+import Link from 'next/link';
 
 function Home() {
- const clickLogout=()=>{}//futur fonction callback logout
+  const dispatch = useDispatch();
+  const user = useSelector((state)=>state.user.value)
+ 
+  const clickLogout=()=>{
+  dispatch(logout());
+ } 
+  // fonction callback logout
+
+const refreshHome =()=>{
+  console.log('click');
+  <Link  href='../pages/home'></Link>
+}
 
   return (
     <div>
       <main className={styles.main}>
       {/* //DIV pour contenir info/logo/button  */}
           <div className={styles.navbar} width={200} height={200} > 
-              <div className={styles.logo}>
-              <Image src="/images/logoHackatweet.png" alt="logo twitter" width={40} height={40}/>
+              <div className={styles.divlogo}>
+              <Image  className={styles.logo} src="/images/logoHackatweet.png" alt="logo twitter" width={40} height={40} onClick={()=>refreshHome()} />
               </div>
             {/* //bouton pour logout */}
+              <p>{user.username}</p>
+              <p>{user.name}</p>
+
             <button className={styles.bouton} onClick={()=>clickLogout()}>logout</button>
           </div>
                <div className={styles.content}>{/*main div for component tweet/lastweets */}
